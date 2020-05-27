0196841aa308573562391e9d85a393d362f209ee
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-05-27 14:03:23 -0500

    complted req 1.b

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -6,11 +6,16 @@
 # Import numPy
 import numpy as np
 
-# Load in iris-training-data.csv into 2 ndarrays
+# Load in iris-training-data.csv into 2 ndarrays we use usecols, and dtype to sepertate the correct data into sepertate arrays of one type
 training_attribute_array = np.loadtxt('iris-training-data.csv', dtype=float, delimiter=',',usecols=(0,1,2,3))
-# We use usecols, and dtype to sepertate the correct data into sepertate arrays of one type
 training_class_lables_array = np.loadtxt('iris-training-data.csv', dtype=str, delimiter=',',usecols=(4))
 
+# Load in iris-testing-data.csv into 2 ndarrays
+testing_attribute_array = np.loadtxt('iris-testing-data.csv', dtype=float, delimiter=',',usecols=(0,1,2,3))
+testing_class_lables_array = np.loadtxt('iris-testing-data.csv', dtype=str, delimiter=',',usecols=(4))
+
 # Tester code to be deleted
-print(training_attribute_array)
-print(training_class_lables_array)
+print("training_attribute_array",training_attribute_array)
+print("training_class_lables_array",training_class_lables_array)
+print("testing_attribute_array",testing_attribute_array)
+print("testing_class_lables_array",testing_class_lables_array)
