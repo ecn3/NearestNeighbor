eeaaa664e93de1de259460c73a1e77759d81db3d
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-05-27 14:01:17 -0500

    completed req 1A read in training to 2 arrays one 1d one 2d

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -3,3 +3,14 @@
 # DATA-51100-002, SUMMER 2020
 # PROGRAMMING ASSIGNMENT #3 Nearest Neighbor
 
+# Import numPy
+import numpy as np
+
+# Load in iris-training-data.csv into 2 ndarrays
+training_attribute_array = np.loadtxt('iris-training-data.csv', dtype=float, delimiter=',',usecols=(0,1,2,3))
+# We use usecols, and dtype to sepertate the correct data into sepertate arrays of one type
+training_class_lables_array = np.loadtxt('iris-training-data.csv', dtype=str, delimiter=',',usecols=(4))
+
+# Tester code to be deleted
+print(training_attribute_array)
+print(training_class_lables_array)
